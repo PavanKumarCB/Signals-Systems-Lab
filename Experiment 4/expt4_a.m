36ad122a64f1c425fe711efb95a483eb89b43a6c
Pavan Kumar C Banasode <pavankumarcbanasode@gmail.com>
2023-06-24 18:18:10 +0530

    Update expt4_a.m

--- Experiment 4/expt4_a.m
+++ Experiment 4/expt4_a.m
@@ -8,7 +8,10 @@ for k=1:N
     X(k)=X(k)+x(n).*exp(-1j.*2.*pi.*(n-1).*(k-1)./N);
   end
 end
-subplot(211); stem(x, abs(X))
+X_mag = abs(X);
+X_ph = angle(X);
+subplot(221); stem(X_mag)
+subplot(222); stem(X_ph)
 
 % IDFT
 Y = zeros(1, N);
@@ -17,4 +20,8 @@ for k=1:n
         Y(k) = Y(k) + (1/N).*X(n).*exp((1j.*2.*pi.*(n-1).*(k-1)./N));
     end
 end
-subplot(212); stem(x, abs(Y))
+Y_mag = abs(Y);
+Y_ph = angle(Y);
+subplot(223); stem(Y_mag)
+subplot(224); stem(Y_ph)
+
