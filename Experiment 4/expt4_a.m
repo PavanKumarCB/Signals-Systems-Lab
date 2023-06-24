35e34d446159dc7fdec5e3db149bd4b96b558344
Pavan Kumar C Banasode <pavankumarcbanasode@gmail.com>
2023-06-24 15:55:30 +0530

    Update expt4_a.m

--- Experiment 4/expt4_a.m
+++ Experiment 4/expt4_a.m
@@ -1,25 +1,20 @@
-% syms k x;
-% N = 3;
 x = [1 2 3 4];
-n = 1:1:4;
-% y = fft(x);
-% mag_y = abs(y); phase_y = angle(y);
-% subplot(311); stem(n,x, 'filled', 'color', 'black');
-% xlabel('n-->'); title('Input discrete signal');
-% 
-% F = 2*pi*n;
-% subplot(312); stem(F/pi, mag_y, 'filled', 'color', 'black');
-% title('Magnitude plot'); xlabel('Frequency in units of pi')
-% 
-% X = ifft(y);
-% subplot(313); stem(F/pi, phase_y, "filled", 'color', 'black');
-% title('Phase plot'); xlabel('Frequency in units of pi')
+N=length(x);
 
-F = f * exp(-1i*(((2*pi))/N)*k);
-y = symsum(F,k,0,N);
-F1 = 2*pi*n;
-mag_y = abs(y); phase_y = angle(y);
-subplot(311); stem(F1,mag_y, 'color', 'black')
+% DFT
+X=zeros(1,N);
+for k=1:N
+  for n=1:N
+    X(k)=X(k)+x(n).*exp(-1j.*2.*pi.*(n-1).*(k-1)./N);
+  end
+end
+subplot(211); stem(x, abs(X))
 
-
-
+% IDFT
+Y = zeros(1, N);
+for k=1:n
+    for n=1:N
+        Y(k) = Y(k) + (1/N).*X(n).*exp((1j.*2.*pi.*(n-1).*(k-1)./N));
+    end
+end
+subplot(212); stem(x, abs(Y))
